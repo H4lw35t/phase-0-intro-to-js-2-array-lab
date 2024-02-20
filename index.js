2c5bcf61ee964d941b915d19156dfe87e93439f5
Halwest Abubakir <halwest.east@gmail.com>
2024-02-20 22:13:47 +0300

    finalllly

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(cat){
+    cats.push(cat)
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat)
+}
+
+function destructivelyRemoveLastCat(cat){
+    cats.pop(cat)
+}
+
+function destructivelyRemoveFirstCat(cat){
+    cats.shift(cat)
+}
+
+function appendCat(Broom){
+    return [...cats, "Broom"]
+}
+
+function prependCat (Arnold){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(name){
+  return  cats.slice(0,2)
+}
+
+function removeFirstCat(name){
+    return cats.slice(1)
+}
